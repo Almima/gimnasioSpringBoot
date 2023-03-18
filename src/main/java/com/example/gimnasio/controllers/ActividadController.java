cb9401f9708b5af234ee2a44804bf4dbbe992381
atralex <atralexrg@gmail.com>
2023-03-18 13:37:37 +0100

    No se ni que he añadido

--- src/main/java/com/example/gimnasio/controllers/ActividadController.java
+++ src/main/java/com/example/gimnasio/controllers/ActividadController.java
@@ -1,11 +1,10 @@
 package com.example.gimnasio.controllers;
 
 import com.example.gimnasio.dao.DaoTipoActividad;
+import com.example.gimnasio.entidades.Instructor;
 import com.example.gimnasio.entidades.TipoActividad;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
 
@@ -14,8 +13,20 @@ public class ActividadController {
     @Autowired
     private DaoTipoActividad daoTipoActividad;
 
-    @RequestMapping(value = "api/getActividadId/{id}")
-    public Optional<TipoActividad> getActividadId(@PathVariable Long id){
+    @RequestMapping(value = "api/getActividadById/{id}")
+    public Optional<TipoActividad> getActividadById(@PathVariable Long id){
         return daoTipoActividad.findById(id);
+    }
+
+    @RequestMapping(value = "api/getActividades")
+    public Iterable<TipoActividad> getActividades(){
+        return daoTipoActividad.findAll();
+    }
+    @RequestMapping(value = "api/registraActividad", method = RequestMethod.POST)
+    public void registraActividad(@RequestBody TipoActividad tipoActividad){
+    }
+    @RequestMapping(value = "api/eliminaActividadById/{id}", method = RequestMethod.DELETE)
+    public void  eliminaActividadById(Long id){
+        daoTipoActividad.deleteById(id);
     }
 }
